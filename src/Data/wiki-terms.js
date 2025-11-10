bb0fd87c713d65442a625886714bd6d1cec6761a
Jon Evert <jonalan620@gmail.com>
2025-11-09 23:52:30 -0800

    fix(build): add JS shim for wiki-terms so astro.config can import

---
+++ src/Data/wiki-terms.js
@@ -0,0 +1,8 @@
+export const WIKI_TERMS = {
+  Deralo: "/deralo/overview/",
+  Luna: "/reference/characters/Luna/",
+  Cortiphage: "/reference/flora/Cortiphage/",
+  Lumestra: "/reference/flora/Lumestra/",
+  Glasvine: "/reference/flora/Glasvine/"
+};
+export default WIKI_TERMS;
